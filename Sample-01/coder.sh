3bba9c09b9464ad13b0866b154c3490a62de1d4e
Yu Sasaki <ysasaki.g.drive@gmail.com>
2023-09-17 10:12:55 +0900

    Update coder.sh

--- Sample-01/coder.sh
+++ Sample-01/coder.sh
@@ -1,1 +1,1 @@
-awk -v domain="$AUTH0_DOMAIN" -v client_id="$AUTH0_APP_CLIENT_ID" -v api_id="$AUTH0_API_ID" \'{gsub("{DOMAIN}", domain); gsub("{CLIENT_ID}", client_id); gsub("{API_IDENTIFIER}", api_id); print}\' ./src/auth_config.json.example > ./src/auth_config.json
+awk -v domain="$AUTH0_DOMAIN" -v client_id="$AUTH0_APP_CLIENT_ID" -v api_id="$AUTH0_API_ID" '{gsub("{DOMAIN}", domain); gsub("{CLIENT_ID}", client_id); gsub("{API_IDENTIFIER}", api_id); print}\' ./src/auth_config.json.example > ./src/auth_config.json
